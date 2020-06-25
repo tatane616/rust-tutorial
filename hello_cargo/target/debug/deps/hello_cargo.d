b9782a1d0dc4ef2aeacc9d9fab7225e125f13c14
tatane <sunnytj39@gmail.com>
2020-06-26 01:01:09 +0900

    Hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/katoukenta/go/github.com/tatane616/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/katoukenta/go/github.com/tatane616/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/katoukenta/go/github.com/tatane616/rust-tutorial/hello_cargo/target/release/hello_cargo: /Users/katoukenta/go/github.com/tatane616/rust-tutorial/hello_cargo/src/main.rs
