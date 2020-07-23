82023ffbbc90195fd2ad776e847b2f2a04fb535e
tatane <sunnytj39@gmail.com>
2020-07-23 18:35:34 +0900

    Drop

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -16,6 +16,16 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.data);
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
@@ -26,6 +36,16 @@ fn main() {
     let m = MyBox::new(String::from("Rust"));
     hello("Rust");
     hello(&m);
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
 fn hello(name: &str) {
