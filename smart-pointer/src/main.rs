76692872c66e01a1fb302c96a65d65efc9ca56de
tatane <sunnytj39@gmail.com>
2020-07-31 00:17:10 +0900

    Chapter15

--- smart-pointer/src/main.rs
+++ smart-pointer/src/main.rs
@@ -26,6 +26,15 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+use std::rc::Rc;
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
@@ -40,12 +49,22 @@ fn main() {
     let c = CustomSmartPointer {
         data: String::from("my stuff"),
     };
-    let d = CustomSmartPointer {
+    let _d = CustomSmartPointer {
         data: String::from("other stuff"),
     };
     println!("CustomSmartPointers created.");
     drop(c);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {
