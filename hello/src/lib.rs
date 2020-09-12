0fba5e8267f3a58664838b2e9d68bb343b58c38c
tatane <sunnytj39@gmail.com>
2020-09-12 23:12:24 +0900

    Signaling to the Threads to stop listening for jobs

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -4,7 +4,7 @@ use std::sync::Mutex;
 use std::thread;
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -38,7 +38,7 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
@@ -48,13 +48,22 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job: executing.", id);
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job: executing.", id);
 
-            job()
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+
+                    break;
+                }
+            }
         });
 
         Worker {
@@ -66,6 +75,14 @@ impl Worker {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+        
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -74,4 +91,9 @@ impl Drop for ThreadPool {
             }
         }
     }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate,
 }
